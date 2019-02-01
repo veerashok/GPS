676b9976c41975cced69febfe4cef8b202189989
veerashok <veerashok163@gmail.com>
2019-02-01 13:45:22 +0530

    Added static files

--- gpsschool/views.py
+++ gpsschool/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    template_name = 'gpsschool/base.html'
+    template_name = 'gpsschool/login.html'
     context = {}
     return render(request, template_name, context)
