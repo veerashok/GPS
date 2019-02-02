56b574cd4a8f3fffcc58583d163cdb65a3631d32
veerashok <veerashok163@gmail.com>
2019-02-02 21:09:17 +0530

    Created new app called accounts for signup

--- GPS/settings.py
+++ GPS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'gpsschool',
 ]
 
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
                     os.path.join(BASE_DIR, 'static'),
 )
+
+LOGIN_REDIRECT_URL = '/'
--- gpsschool/views.py
+++ gpsschool/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    template_name = 'gpsschool/login.html'
+    template_name = 'gpsschool/signup.html'
     context = {}
     return render(request, template_name, context)
